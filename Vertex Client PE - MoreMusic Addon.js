072b15b8dc9d7fe8c4dbf3a98eccf22c4e776996
LPMG <xxlpmghdxx@gmail.com>
2016-10-08 22:00:17 +0200

    Added 5 songs

--- Vertex Client PE - MoreMusic Addon.js
+++ Vertex Client PE - MoreMusic Addon.js
@@ -51,7 +51,12 @@ const Category = {
  *	CUSTOM MODULES
  */
 
-registerSong(new Song("song name", "song artist", "song url")); //example
+//registerSong(new Song("song name", "song artist", "song url")); //example
+registerSong(new Song("Why We Lose (feat. Coleman Trapp) [NCS Release]", "Cartoon", "http://nocopyrightsounds.co.uk/video/cartoon-why-we-lose-feat-coleman-trapp-ncs-release/?download=1")); 
+registerSong(new Song("Eternal Minds [NCS Release]", "Waysons", "http://nocopyrightsounds.co.uk/video/waysons-eternal-minds-ncs-release/?download=1")); 
+registerSong(new Song("Change Your Ways (feat. Charlotte Haining) [NCS Release]", "High Maintenance", "http://nocopyrightsounds.co.uk/video/high-maintenance-change-your-ways-feat-charlotte-haining-ncs-release/?download=1")); 
+registerSong(new Song("Aperture [NCS Release]", "Unison", "http://nocopyrightsounds.co.uk/video/unison-aperture-ncs-release/?download=1")); 
+registerSong(new Song("Vibe [NCS Release]", "Ash O’Connor", "http://nocopyrightsounds.co.uk/video/ash-oconnor-vibe-ncs-release/?download=1")); 
 
 /*
 var exampleModule = {
@@ -61,6 +66,107 @@ var exampleModule = {
 	type: "Mod",
 	state: false,
 	isStateMod: function() {
+29
+​
+30
+/**
+31
+* YOUR ADDON CONTENT
+32
+*/
+33
+​
+34
+const ADDON_NAME = "MoreMusic"; //Your addon's name
+35
+const ADDON_DESC = "Adds many songs into Vertex Client PE."; //Your addon's description
+36
+const ADDON_VERSION = "1.0"; //Your addon's version
+37
+const TARGET_VERSION = "1.6"; //Your addon's target Vertex Client PE version (in this case we use Vertex Client PE v1.0.1)
+38
+​
+39
+var modules = [];
+40
+var songs = [];
+41
+​
+42
+const Category = {
+43
+        COMBAT: 0,
+44
+        BUILDING: 1,
+45
+        MOVEMENT: 2,
+46
+        CHAT: 3,
+47
+        MISC: 4
+48
+}
+49
+​
+50
+/**
+51
+ *      CUSTOM MODULES
+52
+ */
+53
+​
+54
+//registerSong(new Song("song name", "song artist", "song url")); //example
+55
+registerSong(new Song("Why We Lose (feat. Coleman Trapp) [NCS Release]", "Cartoon", "http://nocopyrightsounds.co.uk/video/cartoon-why-we-lose-feat-coleman-trapp-ncs-release/?download=1")); 
+56
+registerSong(new Song("Eternal Minds [NCS Release]", "Waysons", "http://nocopyrightsounds.co.uk/video/waysons-eternal-minds-ncs-release/?download=1")); 
+57
+registerSong(new Song("Change Your Ways (feat. Charlotte Haining) [NCS Release]", "High Maintenance", "http://nocopyrightsounds.co.uk/video/high-maintenance-change-your-ways-feat-charlotte-haining-ncs-release/?download=1")); 
+58
+​
+59
+/*
+60
+var exampleModule = {
+61
+        name: "Example toggleable module",
+62
+        desc: "Example description.",
+63
+        category: Category.MOVEMENT,
+64
+        type: "Mod",
+65
+        state: false,
+66
+        isStateMod: function() {
+67
+                return true;
+68
+        },
+69
+        onToggle: function() {
+70
+                this.state = !this.state;
+71
+                if(this.state) {
+72
+                        //do something when this module is enabled
+73
+                } else {
+74
+                        //do something when this module is disabled
+75
+                }
+76
+        },
+77
+        onTick: function() {
+78
+                //this will get called in modTick when the mod is enabled, also possible: onUseItem, onAttack, onHurt, onChat etcetera (add these functions separated by a comma, like what I did with onUseItem in this module)
+
 		return true;
 	},
 	onToggle: function() {
